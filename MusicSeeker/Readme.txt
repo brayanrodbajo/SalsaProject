cff61802beb7e60041cc244d779bd4e2b5965fbd
brayanrodbajo <bryan951112@gmail.com>
2015-05-18 13:43:02 -0500

    Primera versión en github de MusikSeeker

---
+++ MusicSeeker/Readme.txt
@@ -0,0 +1,5 @@
+1. Para la ejecución de este software, se debe tener eclipse instalado. 
+2. Desde eclipse se importa la carpeta "MusikSeeker".
+3. Se edita el archivo "reconocedor.java" y en la inicialización de variable path se le coloca la ruta de la carpeta donde están los archivos .wav.
+4. Se ejecuta.
+En el archivo "fv.arff" quedan las características extraídas de las canciones.
---
+++ MusicSeeker/src/reconocedor.java
@@ -0,0 +1,106 @@
+import jAudioFeatureExtractor.ACE.XMLParsers.XMLDocumentParser;
+import jAudioFeatureExtractor.AudioFeatures.FeatureExtractor;
+import jAudioFeatureExtractor.DataTypes.RecordingInfo;
+
+import jAudioFeatureExtractor.DataModel;
+import jAudioFeatureExtractor.ModelListener;
+
+import jAudioFeatureExtractor.jAudioTools.AudioMethods;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.util.Vector;
+
+import org.omg.CORBA.portable.OutputStream;
+
+import processing.core.PApplet;
+
+import javazoom.jl.converter.Converter;
+import javazoom.jl.decoder.JavaLayerException;
+
+@SuppressWarnings("serial")
+public class reconocedor  {
+	//TODO Reemplazar la referencia a la ruta donde se encuentran los archivos
+	String path= "/home/family/Musica/muestra";
+	final File folder = new File(path);
+	
+	Vector <RecordingInfo> ri =new Vector <RecordingInfo>();  
+	
+	
+	
+	String metaFinal;
+	AudioMethods metodos;
+	ModelListener ml;  
+	DataModel dm;
+	
+	//MFCC m	fccObj;
+
+	public void setup() {
+		dm = new DataModel ("features.xml", null, "fk_muestra.arff", "fv_muestra.arff");
+		metaFinal = "";
+		try {
+			listFilesForFolder(folder);
+			extract();
+		} catch (Exception e) {
+			System.out.println("excepcion ");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void mp3towav(String sourceFile, String destinationFile) {
+
+		Converter myConverter = new Converter();
+        try {
+			myConverter.convert(sourceFile, destinationFile);
+		} catch (JavaLayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+	@SuppressWarnings("static-access")
+	public void listFilesForFolder(final File folder) throws Exception {
+		int i=0;
+		for (final File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				listFilesForFolder(fileEntry);
+			} else {
+				try {
+					String path =fileEntry.getAbsolutePath();
+					if (path.contains(".mp3")) {
+						// metodos.getAudioFileFormatData(fileEntry);
+						String wav_path = path.substring(0,path.length()-4)+".wav";
+						
+						mp3towav(path, wav_path);
+						ri.add(new RecordingInfo(wav_path));
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				i++;
+			}
+		}
+		
+		
+	}
+	
+	public void extract() throws Exception{
+		dm.extract(512, 0.0, 22.05, false, true, false, ri, 1);
+	}
+	
+	public static void main(String args[]){
+	
+		System.out.println("Ejecución Iniciada");
+		reconocedor recon = new reconocedor();
+		recon.setup();
+		System.out.println("Ejecución Finalizada");
+	}
+	
+
+}
